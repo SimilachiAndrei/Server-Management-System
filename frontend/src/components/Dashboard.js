51d985029cd58acc0c88c3e1e6c026ffd1d9e26b
Similachi Andrei-Felix <sivfhd@yahoo.com>
2024-08-04 16:46:38 +0300

    sample dasboard page

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import '../styles/header.module.css';
-import '../styles/footer.module.css';
-import '../styles/Dashboard.module.css';
-
+import headerStyle from '../styles/header.module.css';
+import footerStyle from '../styles/footer.module.css';
+import pageStyle from '../styles/Dashboard.module.css';
 
 function Dashboard() {
   return (
-    <div className='page'>
-      <h2>Dashboard</h2>
-      <p>Welcome to the dashboard!</p>
+    <div className={pageStyle.page}>
+
+      <div className={headerStyle.header}>
+        <div className={headerStyle.title}>Cockpit</div>
+      </div>
+
+      <div className={pageStyle.content}>
+        <div className={pageStyle.formContainer}>
+          <h3>Computer Panel</h3>
+          <form>
+            <div className={pageStyle['form-group']}>
+              <label>Name</label>
+              <input type='text' name='name'></input>
+            </div>
+            <div className={pageStyle['form-group']}>
+              <label>Description</label>
+              <input type='text' name='description'></input>
+            </div>
+            <div className={pageStyle['form-group']}>
+              <label>IP Address</label>
+              <input
+                type='text'
+                name='ipAddress'
+                pattern="^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
+                title="Please enter a valid IP address (e.g., 192.168.0.1)"
+                required
+              ></input>
+            </div>
+            <button type='submit'>Add</button>
+          </form>
+        </div>
+        <div className={pageStyle.computers}>
+          <div className={pageStyle['computer-container']}>
+            <div className={pageStyle['computer-container-name']}>AndreiPC</div>
+            <div className={pageStyle['computer-container-description']}>Chestie</div>
+            <div className={pageStyle['computer-container-Ip Address']}>127.0.0.1</div>
+            <button>Connect</button>
+          </div>
+        </div>
+      </div>
+
+      <div className={footerStyle.footer}>
+        <div className={footerStyle['footer-content']}>
+          All rights reserved to Similachi Andrei!
+        </div>
+      </div>
+
     </div>
   );
 }
