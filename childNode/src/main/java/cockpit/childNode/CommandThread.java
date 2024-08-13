b2f5445fe4b4bc18c4bd95cc6b53c008da794dea
Similachi Andrei-Felix <sivfhd@yahoo.com>
2024-08-13 15:26:46 +0300

    added CommandThread implementation

--- childNode/src/main/java/cockpit/childNode/CommandThread.java
+++ childNode/src/main/java/cockpit/childNode/CommandThread.java
@@ -1,8 +1,6 @@
 package cockpit.childNode;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 
 public class CommandThread implements Runnable {
@@ -14,8 +12,20 @@ public class CommandThread implements Runnable {
 
     @Override
     public void run() {
-        try (InputStream input = socket.getInputStream();
-             OutputStream output = socket.getOutputStream()) {
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter output = new PrintWriter(socket.getOutputStream(),true)) {
+            while (!socket.isClosed()) {
+                String command = input.readLine().trim();
+                System.out.println(command + "\n");
+                try {
+                    StringBuilder stringBuilder = getStringBuilder(command);
+                    System.out.println(stringBuilder.toString());
+                    output.println(stringBuilder.toString());
+                } catch (IOException | InterruptedException exception) {
+                    System.out.println(exception.getMessage());
+                    output.println("An exception has occured: " + exception.getMessage());
+                }
+            }
         } catch (IOException e) {
             System.out.println("Error handling command: " + e.getMessage());
         } finally {
@@ -25,5 +35,20 @@ public class CommandThread implements Runnable {
                 System.out.println("Error closing socket: " + e.getMessage());
             }
         }
+    }
+
+    private static StringBuilder getStringBuilder(String command) throws InterruptedException, IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c", command);
+        processBuilder.redirectErrorStream(true);
+        Process process = processBuilder.start();
+        BufferedReader processOutput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        while ((line = processOutput.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+        int exitCode = process.waitFor();
+        stringBuilder.append("Process has finished with exit code: ").append(exitCode);
+        return stringBuilder;
     }
 }
